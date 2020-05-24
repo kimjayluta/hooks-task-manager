b5902f3a3911035ec93936c7563824f285943a68
kimjayluta <kimjayluta0721@gmail.com>
2020-05-24 17:57:28 +0800

    chore: Saving the Data to local storage

--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -1,25 +1,16 @@
-import React, { createContext, useState} from 'react'
+import React, { createContext, useState, useEffect} from 'react'
 import {v1 as uuid} from 'uuid'
 
 export const TaskListContext = createContext();
 
 const TaskListContextProvider = props => {
-	const [tasks, setTask] = useState([
-		{
-			title: "Read a Book",
-			id: 1
-		},
-		{
-			title: "Find a Girlfriend",
-			id: 2
-		},
-		{
-			title: "Wash the card",
-			id: 3
-		}
-	]);
-
+	const initialState = JSON.parse(localStorage.getItem('tasks')) || [];
+	const [tasks, setTask] = useState(initialState);
 	const [ editItem, setEditItem ] = useState(null);
+
+	useEffect(() => {
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	}, [tasks]);
 
 	// Adding task functionality
 	const addTask = (title) => {

8c3f4322b515bf88a7d128802925ddd17631dbc9
kimjayluta <kimjayluta0721@gmail.com>
2020-05-24 18:04:20 +0800

    Updates

---
+++ precache-manifest.1d4f154948262b6a27b97bece709706b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183eaca3eaf0a7c610bf87272216904b",
+    "url": "/hooks-task-manager/index.html"
+  },
+  {
+    "revision": "84a2ce398b0d1acbc206",
+    "url": "/hooks-task-manager/static/css/main.53f1dcb8.chunk.css"
+  },
+  {
+    "revision": "1b2f3d721c34e532aa0e",
+    "url": "/hooks-task-manager/static/js/2.2145e015.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks-task-manager/static/js/2.2145e015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a2ce398b0d1acbc206",
+    "url": "/hooks-task-manager/static/js/main.d907192a.chunk.js"
+  },
+  {
+    "revision": "93eae1c11f50921b0b02",
+    "url": "/hooks-task-manager/static/js/runtime-main.dd3cf448.js"
+  }
+]);
